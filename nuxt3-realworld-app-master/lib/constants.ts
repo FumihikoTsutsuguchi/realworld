34527cd243b89770acd92c1dc865cfc1fed312f9
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2025-10-19 21:33:30 +0900

    vue3

--- nuxt3-realworld-app-master/lib/constants.ts
+++ nuxt3-realworld-app-master/lib/constants.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Conduit'
-export const BASE_API_URL = 'https://api.realworld.io/api'
+export const BASE_API_URL = 'http://localhost:8080'
 export const ARTICLES_PER_PAGE = 10
